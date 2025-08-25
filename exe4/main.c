f7ae8e755ca6caedfd9d4a5df9a27dd5b222083f
guedera <guilhermegg5@al.insper.edu.br>
2025-08-25 12:48:09 -0300

    exe4.1

--- exe4/main.c
+++ exe4/main.c
@@ -8,12 +8,17 @@ const int BTN_RED = 28;
 const int BTN_GREEN = 26;
 
 void btn_callback(uint gpio, uint32_t events) {
+    static int led_v_estado = 0;
+    static int led_g_estado = 0;
+    
     if (gpio == BTN_RED && events == GPIO_IRQ_EDGE_FALL) {
         //led v
-        gpio_put(LED_RED, !gpio_get(LED_RED));
+        led_v_estado = !led_v_estado;
+        gpio_put(LED_RED, led_v_estado);
     } else if (gpio == BTN_GREEN && events == GPIO_IRQ_EDGE_RISE) {
         //led g
-        gpio_put(LED_GREEN, !gpio_get(LED_GREEN));
+        led_g_estado = !led_g_estado;
+        gpio_put(LED_GREEN, led_g_estado);
     }
 }
 
@@ -39,8 +44,8 @@ int main() {
     gpio_pull_up(BTN_GREEN);
 
     //interrps
-    gpio_set_irq_enabled_with_callback(BTN_RED, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
-    gpio_set_irq_enabled(BTN_GREEN, GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_irq_enabled_with_callback(BTN_RED, GPIO_IRQ_EDGE_FALL, 1, &btn_callback);
+    gpio_set_irq_enabled(BTN_GREEN, GPIO_IRQ_EDGE_RISE, 1);
 
     while (true) {
     }
