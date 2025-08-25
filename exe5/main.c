21b429347d30dc010a8801175104d03e65f6fea8
guedera <guilhermegg5@al.insper.edu.br>
2025-08-25 13:01:06 -0300

    exe5.2

--- exe5/main.c
+++ exe5/main.c
@@ -6,7 +6,9 @@
 #define LED_PIN 4
 #define TEMPO_LONGO 800
 
-static uint32_t tempo_inicio = 0;
+static volatile uint32_t tempo_inicio = 0;
+static volatile int aperto_curto_flag = 0;
+static volatile int aperto_longo_flag = 0;
 
 void botao_callback(uint gpio, uint32_t events) {
     if (events & GPIO_IRQ_EDGE_FALL) {
@@ -18,9 +20,9 @@ void botao_callback(uint gpio, uint32_t events) {
         uint32_t duracao = tempo_fim - tempo_inicio;
         
         if (duracao >= TEMPO_LONGO) {
-            printf("Aperto longo!\n");
+            aperto_longo_flag = 1;
         } else {
-            printf("Aperto curto!\n");
+            aperto_curto_flag = 1;
         }
     }
 }
@@ -38,6 +40,16 @@ int main() {
     gpio_set_irq_enabled_with_callback(BOTAO_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, 1, &botao_callback);
 
     while (true) {
+        if (aperto_curto_flag) {
+            printf("Aperto curto!\n");
+            aperto_curto_flag = 0;
+        }
+        
+        if (aperto_longo_flag) {
+            printf("Aperto longo!\n");
+            aperto_longo_flag = 0;
+        }
+        
         tight_loop_contents();
     }
 }
