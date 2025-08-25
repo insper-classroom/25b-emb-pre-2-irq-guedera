4058d698cb90bfed868d380dc7f77e87509763e9
guedera <guilhermegg5@al.insper.edu.br>
2025-08-25 12:23:59 -0300

    exe2

--- exe2/main.c
+++ exe2/main.c
@@ -2,9 +2,36 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-int main() {
-  stdio_init_all();
+#define BUTTON_PIN 28
+#define LED_PIN 4
 
-  while (true) {
-  }
+volatile int button_pressed = 0;
+
+void gpio_irq_handler(uint gpio, uint32_t events) {
+    if (gpio == BUTTON_PIN && events & GPIO_IRQ_EDGE_FALL) {
+        button_pressed = 1;
+    }
+}
+
+int main() {
+    stdio_init_all();
+    
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+    
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    
+    bool led_state = false;
+    
+    while (true) {
+        if (button_pressed) {
+            led_state = !led_state;
+            gpio_put(LED_PIN, led_state);
+            button_pressed = 0;
+        }
+    }
 }
